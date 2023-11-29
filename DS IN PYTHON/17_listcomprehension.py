be5e3c9099e9dac89edc812148625ebd643b6b5c
Karan G <96428213+karangnayaka@users.noreply.github.com>
2023-11-29 22:50:47 +0530

    Add files via upload

---
+++ DS IN PYTHON/17_listcomprehension.py
@@ -0,0 +1,13 @@
+#list comprehenion 
+#creating a  new list from the previous list 
+
+prev_list = [1, 2, 3]
+print(prev_list)
+#new_list = [new_item for item in prev_list]
+
+new_list = [i * 2 for i in prev_list]
+print(new_list)
+
+# language = 'python'
+# new_list = [letter for letter in language]
+# print(new_list)
